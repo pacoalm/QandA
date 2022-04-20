46e19038c1cabcb1bef61496a11e7d447dc66519
pacoalm <pacoalm@gmail.com>
2022-04-20 18:43:50 +0100

    Miercoles20/04

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,19 +4,31 @@ import { fontFamily, fontSize, gray2 } from './Styles';
 import React from 'react';
 import { Header } from './header';
 import { HomePage } from './HomePage';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AskPage } from './AskPage';
+import { SearchPage } from './SearchPage';
+import { SignInPage } from './SigInPage';
+import { NotFoundPage } from './NotFoundPage';
 function App() {
   return (
-    <div
-      css={css`
-        font-family: ${fontFamily};
-        font-size: ${fontSize};
-        color: ${gray2};
-      `}
-    >
-      <Header />
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <div
+        css={css`
+          font-family: ${fontFamily};
+          font-size: ${fontSize};
+          color: ${gray2};
+        `}
+      >
+        <Header />
+        <Routes>
+          <Route path="" element={<HomePage />} />
+          <Route path="search" element={<SearchPage />} />
+          <Route path="ask" element={<AskPage />} />
+          <Route path="signin" element={<SignInPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 export default App;
