c3c9b75692d3297d706e41c460f6971d969ba9ec
pacoalm <pacoalm@gmail.com>
2022-05-03 09:25:48 +0100

    03-05Morning

---
+++ Backend/QandA/Controllers/QuestionsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using QandA.Data;
+using QandA.Data.Models;
+
+namespace QandA.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionsController : ControllerBase
+    {
+        private readonly IDataRepository _dataRepository;
+
+        public QuestionsController(IDataRepository dataRepository)
+        {
+            _dataRepository = dataRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<QuestionGetManyResponse> GetQuestions(string search)
+        {
+
+            if (string.IsNullOrEmpty(search))
+            {
+                return _dataRepository.GetQuestions();
+            }
+            else
+            {
+                return _dataRepository.GetQuestionsBySearch(search);
+            }
+            
+        }
+
+    }
+}
---
+++ Backend/QandA/Data/Models/QuestionGetManyResponse.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QandA.Data.Models
+{
+    public class QuestionGetManyResponse
+    {
+        public int QuestionId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string UserName { get; set; }
+        public DateTime Created { get; set; }
+    }
+
+    public class AnswerGetResponse
+    {
+        public int AnswerId { get; set; }
+        public string Content { get; set; }
+        public string UserName { get; set; }
+        public DateTime Created { get; set; }
+    }
+
+    public class QuestionGetSingleResponse
+    {
+        public int QuestionId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string UserName { get; set; }
+        public string UserId { get; set; }
+        public DateTime Created { get; set; }
+        public IEnumerable<AnswerGetResponse> Answers
+        {
+            get;
+            set;
+        }
+    }
+    public class QuestionPostRequest
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public DateTime Created { get; set; }
+    }
+
+    public class QuestionPutRequest
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+
+    public class AnswerPostRequest
+    {
+        public int QuestionId { get; set; }
+        public string Content { get; set; }
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
